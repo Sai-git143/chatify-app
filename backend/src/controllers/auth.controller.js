8926e90fedbd045e2c49c23f7111d34bdba45424
Sai-git143 <sidhinareshsaiaravind143@gmail.com>
2026-02-24 21:35:40 +0530

    signup controller added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,7 +8,9 @@ export const signup=async (req,res)=>{
         if(!fullName ||!email||!password){
             return res.status(400).json({message:"All fields are required"})
         }
-
+        if(password.length <6){
+            return res.status(400).json({message:"Password must be at least 6 characters"})
+        }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
         if (!emailRegex.test(email)) {
