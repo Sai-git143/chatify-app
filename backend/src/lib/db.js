6d532b9904a7208120561dd9fbdbf72e6c921ffb
Sai-git143 <sidhinareshsaiaravind143@gmail.com>
2026-02-24 21:49:09 +0530

    refactor: improve user signup flow and add environment variable checks for JWT and MongoDB

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () =>{
     try{
+        const { MONGODB_URL} = process.env;
+        if(!MONGODB_URL){
+            throw new Error("MONGODB_URL is not defined in environment variables")
+        }
         const conn=await mongoose.connect(process.env.MONGODB_URL)
         console.log("Mongodb is connected:" , conn.connection.host)
     }
